2953fb88041bd0b135b115fc22f9c72b67ee043c
Prince7374 <97323068+Prince7374@users.noreply.github.com>
2024-03-15 10:38:08 +0530

    Add files via upload
    Project developed using HTML5, Tailwind, CSS, Java Script, React.js, PHP, SQL
     a long project of the library management official website that attempt to utilized for the management for
    issue books, return book, email alert and library management system our page.

---
+++ header.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+	</head>
+	<body>
+		<nav class="text-gray-600 body-font bg-gradient-to-r from-pink-100 via-pink-100 to-pink-100">
+		<div class="container mx-auto px-24 flex flex-wrap p-5 flex-col md:flex-row items-center">
+			<a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 cursor-pointer">
+				<img src="img/ic_logo.svg" alt="Logo" />
+				<span class="ml-3 font-bold text-2xl ">My Library</span>
+			</a>
+			<nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+				<a href="http://localhost/library_pro/PROJECT%20LIBRARY/index1.php" class="mr-5 text-gray-600 font-bold text-xl hover:text-gray-900">Home</a>
+				<a href="http://localhost/library_pro/PROJECT%20LIBRARY/books.php" class="mr-5 text-gray-600 font-bold text-xl hover:text-gray-900">Books</a>
+				<a href="http://localhost/library_pro/PROJECT%20LIBRARY/contact.php" class="mr-5 text-gray-600 font-bold text-xl hover:text-gray-900">Contact</a>
+				<a href="http://localhost/library_pro/PROJECT%20LIBRARY/aboutus.php" class="mr-5 text-gray-600 font-bold text-xl hover:text-gray-900">About</a>
+			</nav>
+		</div> 
+	</nav>
+	</body>
+</html>
